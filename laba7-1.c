8459ab197e3875b1655df588e2d7f5bdb1f51085
Lerok <ovseychuk2000@mail.ru>
2020-05-12 14:43:04 +0200

    LAB_7 changed

--- laba7-1.c
+++ laba7-1.c
@@ -7,15 +7,19 @@
 
 int main(void)
 {
-	time_t timer = time(NULL);  //Считываем системное время
-	sem_t *sem1 = sem_open("sem1", O_CREAT, 0777, timer);
-	sem_t *sem2 = sem_open("sem2", O_CREAT, 0777, 0);
 
-	sem_getvalue(sem1, &timer);  
-	sem_post(sem1);
+	time_t timer = time(NULL);  //Считываем системное время
+
+	sem_t *sem1 = sem_open("sem1", O_CREAT, 0777, 0);
+	sem_t *sem2 = sem_open("sem2", O_CREAT, 0777, 0);
+while(1){
+	//sem_wait(sem2);
+	timer = time(NULL);
+	//sem_getvalue(sem1, &timer);
 	printf("process sends current time: %ld\n", timer);
-	sem_wait(sem2);
-	
+	sem_post(sem1); sem_post(sem2);
+	}
+
 	sem_unlink("sem1");
 	sem_unlink("sem2");
 	return(0);
--- laba7-2.c
+++ laba7-2.c
@@ -7,17 +7,23 @@
 #include <string.h>
 
 int main(void) {
+
 	sem_t *sem1 = sem_open("sem1", O_CREAT, 0777, 0);
 	sem_t *sem2 = sem_open("sem2", O_CREAT, 0777, 0);
-	time_t timer = 0; char str[128]="";
-
+	time_t timer = time(NULL); 
+	 char str[128]="";
+while (1){
+	sem_wait(sem2);
         sem_getvalue(sem1, &timer); //получаем значение семафора
+	int counter = (int)timer;
  	timer = localtime (&timer); //преобразуем системное время в локальное
    	strftime (str, 128, "%x %A %X", timer); //преобразуем локальное время в текстовую строку
         printf("process gets current time: %s \n", str);
-
-        sem_post(sem2);
-
+	while (counter != 0) {
+	sem_wait(sem1);
+	counter--;
+	}
+}
 	sem_unlink("sem1");
 	sem_unlink("sem2");
 	return 0;
